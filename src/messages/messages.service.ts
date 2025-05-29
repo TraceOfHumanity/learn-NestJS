b2535e8cc90dee522a28b22a6b6b5cccc3a8a5fc
TraceOfHumanity <azalexxx02@gmail.com>
2025-05-29 23:43:23 +0300

    feat: add new message and refactor MessagesController, Service, and Repository for improved dependency injection

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,10 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepository: MessagesRepository;
-  constructor() {
-    this.messagesRepository = new MessagesRepository();
-  }
+  constructor(private messagesRepository: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepository.findOne(id);
