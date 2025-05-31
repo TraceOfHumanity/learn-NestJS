c5cf6c06a4fed0e2fdf26fdada240279d28c26a6
TraceOfHumanity <azalexxx02@gmail.com>
2025-06-01 02:04:53 +0300

    feat: enhance user entity and service with lifecycle hooks and additional methods for user management

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, BeforeUpdate, BeforeRemove } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @BeforeUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @BeforeRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
