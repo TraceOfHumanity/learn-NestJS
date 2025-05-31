77cd1cd4d7b95410e8267550b3ee76f743a70d5a
TraceOfHumanity <azalexxx02@gmail.com>
2025-06-01 02:27:22 +0300

    feat: implement user management features including retrieval, update, and deletion of users

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
+import { UpdateUserDto } from "./dtos/update-user.dto";
 
 @Controller('auth')
 export class UsersController {
@@ -8,7 +9,53 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.usersService.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto) {
+    const user = await this.usersService.create(body.email, body.password);
+
+    return user;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    const users = await this.usersService.find(email);
+
+    if (!users.length) {
+      throw new NotFoundException('users not found');
+    }
+
+    return users;
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id: string) {
+    const user = await this.usersService.remove(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const user = await this.usersService.update(parseInt(id), body);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
   }
 }
