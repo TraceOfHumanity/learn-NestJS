ca5a38494aba7d8b33d5ba84cf6b1328f0b09858
TraceOfHumanity <azalexxx02@gmail.com>
2025-06-01 17:16:17 +0300

    feat: integrate AuthService into UsersController for user authentication and registration

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -14,17 +14,24 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
-    const user = await this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
+  }
 
-    return user;
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 
   @Get('/:id')
